d2f32cb1e19b26817f8511f8762849522ce4be34
Pedro Vitor Emanuel <ped.frsouza@gmail.com>
2025-08-22 23:20:39 -0300

    day 3

---
+++ Arrays/transformsEachElement.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let newArray = new Array(arr.length);
+
+    for ( const i in arr ){
+        newArray[i] =  fn(arr[i], Number(i));
+    }
+    return newArray;
+
+};
